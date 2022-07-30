92ea98ab6bd5232de31fdf22592fcc69aa7988ec
Louis Durieux <louis.durieux99@gmail.com>
2022-07-30 21:16:28 +0200

    add ft_strcmp

---
+++ srcs/ft_strcmp.c
@@ -0,0 +1,14 @@
+#include "../includes/ft_string.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	register const unsigned char	*r1 = (unsigned char *)s1;
+	register const unsigned char	*r2 = (unsigned char *)s2;
+
+	while (*r1 == *r2 && *r1)
+	{
+		r1++;
+		r2++;
+	}
+	return (*r1 - *r2);
+}
