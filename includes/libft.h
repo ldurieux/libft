7a68ee17c1552cbdd357b93e087e35080214d88e
Louis Durieux <loudurserveur@gmail.com>
2022-08-12 02:29:46 +0200

    add ft_stricmp ft_strnicmp && fix ft_strncmp

--- includes/libft.h
+++ includes/libft.h
@@ -48,6 +48,8 @@ t_size		ft_strnlen(const char *str, t_size n);
 
 int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, t_size n);
+int			ft_stricmp(const char *s1, const char *s2);
+int			ft_strnicmp(const char *s1, const char *s2, t_size n);
 
 char		*ft_strchr(char *str, char chr);
 char		*ft_strrchr(char *str, char chr);
--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -30,10 +30,56 @@ int	ft_strncmp(const char *s1, const char *s2, t_size n)
 	register const unsigned char	*r1 = (unsigned char *)s1;
 	register const unsigned char	*r2 = (unsigned char *)s2;
 
-	while (*r1 == *r2 && *r1 && n-- < 1)
+	if (n == 0)
+		return (0);
+	while (*r1 == *r2 && *r1 && n-- > 1)
 	{
 		r1++;
 		r2++;
 	}
 	return (*r1 - *r2);
+}
+
+static inline unsigned char	ft_chrlwr(unsigned char c)
+{
+	t_bool	cmp;
+
+	cmp = (c >= 'A') * (c <= 'Z');
+	return (c * !cmp + (c + 32) * cmp);
+}
+
+int	ft_stricmp(const char *s1, const char *s2)
+{
+	register const unsigned char	*r1 = (unsigned char *)s1;
+	register const unsigned char	*r2 = (unsigned char *)s2;
+	unsigned char					a;
+	unsigned char					b;
+
+	while (1)
+	{
+		a = ft_chrlwr(*r1++);
+		b = ft_chrlwr(*r2++);
+		if (a != b || !a)
+			break ;
+	}
+	return (a - b);
+}
+
+int	ft_strnicmp(const char *s1, const char *s2, t_size n)
+{
+	register const unsigned char	*r1 = (unsigned char *)s1;
+	register const unsigned char	*r2 = (unsigned char *)s2;
+	unsigned char					a;
+	unsigned char					b;
+
+	if (n == 0)
+		return (0);
+	while (1)
+	{
+		a = ft_chrlwr(*r1++);
+		b = ft_chrlwr(*r2++);
+		if (a != b || !a || n-- > 1)
+			break ;
+	}
+	return (a - b);
 }
