d6af45f29024a2f6a1c400657add791531aa1b78
Louis Durieux <louis.durieux99@gmail.com>
2022-09-01 14:42:57 +0200

    fix forgot prototype in libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -293,6 +293,7 @@ t_list		*ft_lstlast(t_list *lst);
 void		ft_lstdelone(t_list *lst, void (*del)(void*));
 void		ft_lstclear(t_list **lst, void (*del)(void*));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 /* --- INLINES --- */
 /* --- CHARACTER TYPE --- */
