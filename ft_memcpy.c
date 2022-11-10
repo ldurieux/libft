257501e22c25cdb8bf87b8ba6334a794ab98b126
Louis Durieux <ldurieux@z2r12p2.42lyon.fr>
2022-11-10 15:41:06 +0100

    simplify memcpy for moulinette

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -15,24 +15,12 @@
 void	*ft_memcpy(void *dst, const void *src, size_t len)
 {
 	size_t			i;
-	t_uint64		*i_dst;
-	const t_uint64	*i_src;
 	char			*c_dst;
 	const char		*c_src;
 
 	if (!dst || !src)
 		return (NULL);
-	i = -1;
-	if ((t_uint64)dst % sizeof(t_uint64) == 0
-		&& (t_uint64)src % sizeof(t_uint64) == 0
-		&& len % sizeof(t_uint64) == 0)
-	{
-		i_dst = (t_uint64 *)dst;
-		i_src = (const t_uint64 *)src;
-		while (++i < len / sizeof(t_uint64))
-			i_dst[i] = i_src[i];
-		return (dst);
-	}
+	i = (size_t)-1;
 	c_dst = (char *)dst;
 	c_src = (const char *)src;
 	while (++i < len)
